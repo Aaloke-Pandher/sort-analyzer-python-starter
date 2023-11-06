eaf40cbe4992fa27fbb245cf55571bc77cf9a5d9
Aaloke-Pandher <98190131+Aaloke-Pandher@users.noreply.github.com>
2023-11-06 13:30:01 -0700

    Update sort-analyzer.py

--- sort-analyzer.py
+++ sort-analyzer.py
@@ -23,12 +23,44 @@ reversedData = loadDataArray("data-files/reversed-values.txt")
 nearlySortedData = loadDataArray("data-files/nearly-sorted-values.txt")
 fewUniqueData = loadDataArray("data-files/few-unique-values.txt")
 
+# Functions 
+def main():
+    print("Goat Lunch")
+# Insertion Sort
+def insertionSort(list):
+    for i in range(1, len(list)):
+        insertVal = list[i] 
+        insertPos = i - 1  
+
+        while(insertPos >= 0 and list[insertPos] > insertVal):
+            list[insertPos + 1] = list[insertPos]
+            insertPos -= 1
+        list[insertPos + 1] = insertVal 
+
+# Selection Sort 
+def selectionSort(list):
+    for i in range(len(list)):
+        minPosition = i 
+        for j in range(minPosition + 1, len(list)): 
+            if list[j] < list[minPosition]:
+                minPosition = j
+        list[minPosition], list[i] = list[i], list[minPosition]  
+
+# Bubble Sort 
+def bubbleSort(list):
+    listLength = len(list)
+    for i in range(listLength - 1): 
+        for j in range(listLength - i - 1): 
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j] 
+
 # VERIFY LOADED DATA BY PRINTING FIRST 50 ELEMENTS
 print(randomData[0:50])
 print(reversedData[0:50])
 print(nearlySortedData[0:50])
 print(fewUniqueData[0:50])
 
+main()
 
 # EXAMPLE OF HOW TO TIME DURATION OF A SORT ALGORITHM
 # startTime = time.time()
